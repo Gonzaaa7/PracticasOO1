e3702b6877b4dc9b5a4951f85df178c27b8cc8b6
Gonza <89936078+Gonzaaa7@users.noreply.github.com>
2025-09-09 12:11:54 -0300

    Add files via upload

---
+++ ej2BalanzaDIjital/src/main/java/oo1/practica1/ej2/ej2BalanzaDIjital/Balanza.java
@@ -0,0 +1,17 @@
+package oo1.practica1.ej2.ej2BalanzaDIjital;
+
+public class Balanza {
+	private int cantidadDeProductos;
+	private double precioTotal;
+	private double pesoTotal;
+}
+
+public void ponerEnCero() {
+	
+}
+public void agregarProducto(Producto pro) {
+	this.cantidadDeProductos++;
+}
+public Ticket emitirTicket() {
+	return Ti
+}
---
+++ ej2BalanzaDIjital/src/main/java/oo1/practica1/ej2/ej2BalanzaDIjital/Ticket.java
@@ -0,0 +1,5 @@
+package oo1.practica1.ej2.ej2BalanzaDIjital;
+
+public class Ticket {
+
+}
